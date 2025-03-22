18e504a83fcc4730a4749f863d95193d08891338
Karl Kolibri <karlkolibri@web.de>
2025-03-22 12:39:27 +0100

    terra

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -1,27 +1,32 @@
-resource "azurerm_network_interface" "" {
-  name                = ""
-  location            = ""
-  resource_group_name = ""
+resource "azurerm_network_interface" "test" {
+  name                = "nic-1"
+  location            = var.location
+  resource_group_name = var.resource_group
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = ""
+    subnet_id                     = var.subnet_id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = ""
+    public_ip_address_id          = var.public_ip
   }
 }
 
-resource "azurerm_linux_virtual_machine" "" {
-  name                = ""
-  location            = ""
-  resource_group_name = ""
-  size                = "Standard_DS2_v2"
-  admin_username      = ""
-  network_interface_ids = []
+
+
+resource "azurerm_linux_virtual_machine" "ubuntu-vm" {
+  name                = "ubuntu18vm"
+  location            = var.location
+  resource_group_name = var.resource_group
+  size                = "Standard_B1s"
   admin_ssh_key {
-    username   = ""
-    public_key = "file("~/.ssh/id_rsa.pub")"
-  }
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+    }
+  
+  disable_password_authentication = false
+  network_interface_ids = [azurerm_network_interface.test.id]
+
+
   os_disk {
     caching           = "ReadWrite"
     storage_account_type = "Standard_LRS"
